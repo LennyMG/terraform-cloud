856a66c285f70427afd11485085190030da91351
LennyMG <107425277+LennyMG@users.noreply.github.com>
2022-09-17 15:13:59 -0500

    vpc

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,24 +1,18 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+  name = "single-instance"
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
 
   tags = {
-    Name = "HelloWorld"
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
